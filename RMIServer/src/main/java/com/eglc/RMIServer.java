ebcfabead366eb822fa8e28fca999eebfd65c94f
jargonfire <53142681@qql.com>
2024-07-11 16:31:09 +0800

    4948

--- RMIServer/src/main/java/com/eglc/RMIServer.java
+++ RMIServer/src/main/java/com/eglc/RMIServer.java
@@ -9,7 +9,7 @@ public class RMIServer {
 
     public static void main(String[] args) throws RemoteException, AlreadyBoundException {
         IRemoteObj remoteObj = new IRemoteObjImpl();
-
+        //按照之前jdk的版本，注册中心是需要在另外一台机器上创建的，jdk几点几之后，为了方便，注册中心直接在服务器上了默认。
         Registry r = LocateRegistry.createRegistry(1099);
 
         r.bind("remoteObj",remoteObj);
